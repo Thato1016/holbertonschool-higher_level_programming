c399786c1d6a3977814a8f0e0e10b54b740d7bc9
Thato1016 <98318441+Thato1016@users.noreply.github.com>
2022-06-01 20:50:50 +0200

    I-O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Contains the read_file function
-"""
+
+'''Task 00 - 0. Read file'''
 
 
 def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
+    '''function that reads a text file (UTF8)
+    and prints it to stdout:'''
+    with open(filename, mode='r', encoding='utf-8') as f:
         print(f.read(), end="")
